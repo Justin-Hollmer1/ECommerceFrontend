7cd040c667afe227c105ea44498aa3fb4e4c0696
Justin Hollmer <justin.hollmer@gmail.com>
2022-11-10 14:38:53 -0600

    gotta go fix CORS stuff again

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,7 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function RegisterForm() {
+
+    let userObject = {
+        "username": document.getElementById("username"),
+        "email": document.getElementById("email"),
+        "password": document.getElementById("password"),
+        "admin_status": 0
+    }
+    function clickToRegister() {
+        // axios.post("ec2-18-224-4-73.us-east-2.compute.amazonaws.com:8080/save-user", userObject);
+        // navigate("/login")
+        // axios.get("http://ec2-18-224-4-73.us-east-2.compute.amazonaws.com:8080/users")
+        //     .then(data => {
+        //         console.log(data)
+        //     })
+    }
+
+
+
+
+
+
+
     let navigate = useNavigate();
     return (
         <div className="register-form">
@@ -13,9 +36,9 @@ function RegisterForm() {
                 <input id="username" name="username" placeholder="username here" />
                 <label htmlFor="password">Password: </label>
                 <input id="password" name="password" placeholder="Enter password" />
-                <label htmlFor="password">Confirm Password: </label>
-                <input id="password" name="password" placeholder="Re-Enter password" />
-                <button id="register-form-register-button" onClick={() => navigate("/login")}>Register</button>
+                <label htmlFor="confirm_password">Confirm Password: </label>
+                <input id="confirm_password" name="confirm_password" placeholder="Re-Enter password" />
+                <button id="register-form-register-button" onClick={clickToRegister}>Register</button>
             </div>
         </div>
     )
